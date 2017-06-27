8422c1964b28d914bb7fbf597afda81f264e898a
Volhan Salai <volhansalai@gmail.com>
2017-06-27 22:55:19 +0200

    feat(DetailsViewController): DetailsViewController setup

--- QRScanner/DetailsViewController.swift
+++ QRScanner/DetailsViewController.swift
@@ -9,27 +9,17 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    var website: String?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openPdf(_ sender: Any) {
+        let pdfWebViewController = self.storyboard?.instantiateViewController(withIdentifier: "PDFWebViewController") as! PDFWebViewController
+        pdfWebViewController.website = website
+        self.navigationController?.pushViewController(pdfWebViewController, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-    */
-
 }
